f92f997bfa79c734e3c3bd353f2ef90dacdc29da
Navraj Chohan <nlake44@gmail.com>
2020-03-16 17:16:28 +0000

    Add axios for url fetching

---
+++ script.sh
@@ -0,0 +1,9 @@
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install react-radio-buttons --save
+npm install @material-ui/core
+npm install @material-ui/icons
+npm install typeface-roboto --save
+npm install react-router-dom
+npm install axios --save
+npm audit fix
